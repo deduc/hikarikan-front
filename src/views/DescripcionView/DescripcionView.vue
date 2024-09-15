cd963113ed45d4274d264122287d9174997ecf68
deduc <deductivegoose@outlook.es>
2024-09-15 18:17:45 +0200

    añadir html

--- src/views/DescripcionView/DescripcionView.vue
+++ src/views/DescripcionView/DescripcionView.vue
@@ -1,10 +1,33 @@
 <template>
-    <h1 id="Karate">¿Qué es el Karate?</h1>
-    <p></p>
-    <h1 id="Kobudo">¿Qué es el Kobudo?</h1>
-    <p></p>
+    <section v-for="arteMarcial in artesMarciales">
+        <article>
+            <div>
+                <h1>{{ arteMarcial.titulo }}</h1>
+                <p v-for="texto in arteMarcial.descripcion">{{ texto }}</p>
+            </div>
+        </article>
+    </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { descripcionText } from "@/core/constants/textos";
 
-<style scoped lang="scss"></style>
+let artesMarciales = descripcionText.artesMarciales;
+</script>
+
+<style scoped lang="scss">
+section article {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    h1 {
+        font-size: 24px;
+    }
+
+    p {
+        padding-left: 32px;
+        padding-right: 32px;
+    }
+}
+</style>
